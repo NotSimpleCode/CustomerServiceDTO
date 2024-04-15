038914b56ecaa0e57856b9669cdece6339cb986e
danieltorrez29 <daniel.torres04@uptc.edu.co>
2024-04-14 20:38:44 -0500

    MySQL Schema Script Deleted & DeleteMapping Annotation Update

--- customerservicedto/src/main/java/co/edu/uptc/swii/customerservicedto/controller/CustomerCatalogueController.java
+++ customerservicedto/src/main/java/co/edu/uptc/swii/customerservicedto/controller/CustomerCatalogueController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +44,7 @@ public class CustomerCatalogueController {
         return new ResponseEntity<>(customerItems, HttpStatus.OK);
     }
 
-    @GetMapping("/deletecustomer/{id}")
+    @DeleteMapping("/deleteCustomer/{id}")
     public ResponseEntity<String> deleteCustomer(@PathVariable Integer customerId) {
         String response = customerCatalogueService.deleteCustomer(customerId);
         return new ResponseEntity<>(response, HttpStatus.OK);
--- customerservicedto/src/main/resources/templates/schema-mysqlDB.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS customer;
-CREATE TABLE customer(
-    id INT PRIMARY KEY,
-    first_name VARCHAR(200) NOT NULL,
-    last_name VARCHAR(200) NOT NULL,
-    address VARCHAR(200)
-);
